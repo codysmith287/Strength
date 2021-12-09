36df9ca32329ec99cf11091f98042c9b433b46ca
codysmith287 <csmith@shift3tech.com>
2021-12-09 00:58:33 -0800

    feat(GlobalStyles.tsx): deleted styles/globals.css and replace with the styled components equivalent

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from '../styles/GlobalStyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
